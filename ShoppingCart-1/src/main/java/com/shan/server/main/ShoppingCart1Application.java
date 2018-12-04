a25f45d15f29216601e978fd174d46094320b471
Shan <srikishan2000@gmail.com>
2018-12-04 04:07:22 +0200

    sample shopping cart initial

---
+++ ShoppingCart-1/src/main/java/com/shan/server/main/ShoppingCart1Application.java
@@ -0,0 +1,12 @@
+package com.shan.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCart1Application.class, args);
+	}
+}
